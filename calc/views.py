0e95d5787aaefa1764e04fed14e2ad359ccea06f
local <local@local>
2017-05-28 15:21:34 -0600

    Prep for adding calc features

--- calc/views.py
+++ calc/views.py
@@ -9,7 +9,7 @@ from .models import calc_log
 
 def index(request):
     # 5 most recent logs
-    calc_log_list = calc_log.objects.order_by('-calc_time')[:5]
+    calc_log_list = calc_log.objects.order_by('-calc_time')
     context = {
         'calc_log_list': calc_log_list
     }
