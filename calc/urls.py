8de502c17d43ce8a9896f095604c74a0c4f22a8e
local <local@local>
2017-05-27 22:37:50 -0600

    A bit ugly, but sorta functional

--- calc/urls.py
+++ calc/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^submit/$', views.test_in, name='test_in'),
 ]
--- calc/views.py
+++ calc/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
-from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+
+from django.utils import timezone
 
 from .models import calc_log
 
@@ -12,3 +14,11 @@ def index(request):
         'calc_log_list': calc_log_list
     }
     return render(request, 'calc/index.html', context)
+
+
+def test_in(request):
+    in_string = request.POST['in_string']
+    op_a = float(in_string)
+    new_rec = calc_log(op_a=op_a, op='*', op_b=0, res=0, calc_time=timezone.now())
+    new_rec.save()
+    return redirect('/calc/')
