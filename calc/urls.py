853fee997afed7ad7310ed0dbcbc4066adf92732
local <local@local>
2017-05-28 23:35:12 -0500

    Unsuccessfully tried to implement input sizing - going to learn grid first

--- calc/urls.py
+++ calc/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 app_name = 'calc'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='generic_index'),
     url(r'^boot_submit/$', views.boot_submit, name='boot_submit'),
-    url(r'^generic_index/$', views.IndexView.as_view(), name='generic_index'),
 ]
--- calc/views.py
+++ calc/views.py
@@ -1,10 +1,59 @@
-from django.shortcuts import get_object_or_404, render, redirect
-from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 
 from .models import calc_log
+
+
+class IndexView(generic.ListView):
+    template_name = 'calc/index.html'
+    context_object_name = 'calc_log_list'
+
+    def get_queryset(self):
+        return calc_log.objects.order_by('-calc_time')
+
+
+def boot_submit(request):
+    print(request.POST)
+    if 'submit' in request.POST:
+        try:
+            op_a = request.POST['op_a']
+            op = request.POST['op']
+            op_b = request.POST['op_b']
+            op_a = float(op_a)
+            op_b = float(op_b)
+
+            if op == "+":
+                res = op_a + op_b
+            elif op == "-":
+                res = op_a - op_b
+            elif op == "*":
+                res = op_a * op_b
+            elif op == "/":
+                res = op_a / op_b
+            elif op == "^":
+                res = op_a ** op_b
+            new_rec = calc_log(op_a=op_a, op=op, op_b=op_b, res=res,
+                               calc_time=timezone.now())
+            new_rec.save()
+        except:
+            context = {
+                'calc_log_list': calc_log.objects.order_by('-calc_time'),
+                'error_message': "Input Not Valid - Please Try Again",
+            }
+            return render(request, 'calc/index.html', context)
+
+    elif 'clear' in request.POST:
+        new_rec = calc_log(op_a=0, op='c', op_b=0, res=0,
+                           calc_time=timezone.now())
+        new_rec.save()
+
+    # Used instead of redirect so that back button goes back to calc page
+    return HttpResponseRedirect(reverse('calc:generic_index'))
+
+# Depreciated
 
 
 def index(request):
@@ -19,40 +68,3 @@ def index(request):
         'calc_log_list': calc_log_list
     }
     return render(request, 'calc/index.html', context)
-
-
-class IndexView(generic.ListView):
-    template_name = 'calc/index.html'
-    context_object_name = 'calc_log_list'
-
-    def get_queryset(self):
-        return calc_log.objects.order_by('-calc_time')
-
-
-def boot_submit(request):
-    print(request.POST)
-    if 'submit' in request.POST:
-        op_a = request.POST['op_a']
-        op = request.POST['op']
-        op_b = request.POST['op_b']
-        op_a = float(op_a)
-        op_b = float(op_b)
-
-        if op == "+":
-            res = op_a + op_b
-        elif op == "-":
-            res = op_a - op_b
-        elif op == "*":
-            res = op_a * op_b
-        elif op == "/":
-            res = op_a / op_b
-        elif op == "^":
-            res = op_a ** op_b
-        new_rec = calc_log(op_a=op_a, op=op, op_b=op_b, res=res, calc_time=timezone.now())
-        new_rec.save()
-        # Used instead of redirect so that back button goes back to calc page
-    elif 'clear' in request.POST:
-        new_rec = calc_log(op_a=0, op='c', op_b=0, res=0, calc_time=timezone.now())
-        new_rec.save()
-
-    return HttpResponseRedirect(reverse('calc:generic_index'))
