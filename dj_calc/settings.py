e655461a6c5759cb2f627882454c6696b100b9c5
local <local@local>
2017-06-19 08:32:13 -0500

    Readded debug mode

--- dj_calc/settings.py
+++ dj_calc/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import dj_database_url
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+
 from .secret_settings import secret_key
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
