47bd61ec867aab47275f4c17c3e4325b035b27e4
local <local@local>
2017-06-19 08:20:40 -0500

    Uncommented database

--- dj_calc/settings.py
+++ dj_calc/settings.py
@@ -87,8 +87,8 @@ DATABASES = {
 }
 
 # Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
